7aefd7b0661289872167251196e8b31f444385cd
Mg Htoo <whshwe@gmail.com>
2024-09-30 15:35:42 +0630

    ()n: buildAnswerButtons -> buildMultipleChoiceButons

--- N5/vocab/assets/js/vocab.js
+++ N5/vocab/assets/js/vocab.js
@@ -107,7 +107,7 @@ function answerManager() {
         content: 'Check Answer', 
         className: 'answer-btn', 
         idName: 'answer-btn', 
-        eventFunction: buildAnswerButtons 
+        eventFunction: buildMultipleChoiceButtons 
       });
     } else { // if it is a multiple choice game
       buildNode({ 
@@ -251,8 +251,8 @@ function answerManager() {
     return array;
   }
 
-  function buildAnswerButtons() {
-    //console.groupCollapsed("answerManager() - buildAnswerButtons()");
+  function buildMultipleChoiceButtons() {
+    //console.groupCollapsed("answerManager() - buildMultipleChoiceButtons()");
 
     // Remove exiting buttons
     const answerButtons = document.querySelectorAll('[id^="answer-btn"]'); // sn3
