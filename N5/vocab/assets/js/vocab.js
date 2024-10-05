93617f146119c2f825c174392cd226d4576dd24a
Mg Htoo <whshwe@gmail.com>
2024-10-05 10:02:13 +0630

    JS: runtimeError() rewritten with switch

--- N5/vocab/assets/js/vocab.js
+++ N5/vocab/assets/js/vocab.js
@@ -473,58 +473,60 @@ function errorManager() {
   function runtimeError(errcode) {
     console.groupCollapsed("runtimeError()")
     console.info("errcode: ", errcode);
-    
-    if (codeMapping[errcode] === "infiniteloop") {
-      let selectedArray = answerMgr.vocabMapping[selectors.aChoice.value];
-      let choiceInput = parseInt(appState.noOfAnswers, 10);
-      let noOfChoice = Math.min(choiceInput, selectedArray.length);
-      
-      console.info("selectedArray :", selectedArray);
-      console.info("choiceInput :", choiceInput);
-      console.info("noOfChoice: ", noOfChoice);
-      
-      // Infinite Loop Prevention: If selectedArray contains very few elements, 
-      // the loop inside do...while of `createAnswerArray()` could run infinitely because it’s trying to pick a unique answer from a small pool, 
-      // but keeps failing due to duplicates. This is less likely, but worth checking.
-  
-      if (selectedArray.length <= noOfChoice) {
-        console.error("Not enough unique answers to generate.");
 
-        if (!document.querySelector("[id|='runtime-error']")) { // if error is not already shown
+    switch (codeMapping[errcode]){
+      case "infiniteloop":
+        let selectedArray = answerMgr.vocabMapping[selectors.aChoice.value];
+        let choiceInput = parseInt(appState.noOfAnswers, 10);
+        let noOfChoice = Math.min(choiceInput, selectedArray.length);
+
+        console.info("selectedArray :", selectedArray);
+        console.info("choiceInput :", choiceInput);
+        console.info("noOfChoice: ", noOfChoice);
+
+        // Infinite Loop Prevention: If selectedArray contains very few elements, 
+        // the loop inside do...while of `createAnswerArray()` could run infinitely because it’s trying to pick a unique answer from a small pool, 
+        // but keeps failing due to duplicates. This is less likely, but worth checking.
+
+        if (selectedArray.length <= noOfChoice) {
+          console.error("Not enough unique answers to generate.");
+
+          if (!document.querySelector("[id|='runtime-error']")) { // if error is not already shown
             showError({
-              errcode: "iLoop", 
-              parentName: selectors.settingNoOfAns, 
+              errcode: "iLoop",
+              parentName: selectors.settingNoOfAns,
               idName: "runtime-error"
             });
           }
-        console.groupEnd();
-        return false; // Return false when there is an error
-      } else {
-        console.info("No runtime error: good to go!");
-        console.groupEnd();
-        return true; // Return true when there is no error
-      }
-    }
-    
-    else if(codeMapping[errcode] === "syllable-error") {
-      if (!document.querySelector("[id|='syllable-error']")) { // if error is not already shown
-        errorInstance.showError({
-          errcode: "noSL",
-          parentName: selectors.settingSyllable,
-          idName: 'syllable-error',
-        });
-      }
-      return false; // Return false for syllable error case
-    }
+          console.groupEnd();
+          return false; // Return false when there is an error
+        } else {
+          console.info("No runtime error: good to go!");
+          console.groupEnd();
+          return true; // Return true when there is no error
+        }
+        break;
 
-    else if (codeMapping[errcode] === "no-stored-data") {
-      if (!document.querySelector("[id|='memory-empty-error']")) { // if error is not already shown
-        showError({
-          errcode: "mem0",
-          parentName: selectors.settingRepractice,
-          idName: "memory-empty-error"
-        });
-      }
+      case "syllable-error":
+        if (!document.querySelector("[id|='syllable-error']")) { // if error is not already shown
+          errorInstance.showError({
+            errcode: "noSL",
+            parentName: selectors.settingSyllable,
+            idName: 'syllable-error',
+          });
+        }
+        return false; // Return false for syllable error case
+        break;
+
+      case "no-stored-data":
+        if (!document.querySelector("[id|='memory-empty-error']")) { // if error is not already shown
+          showError({
+            errcode: "mem0",
+            parentName: selectors.settingRepractice,
+            idName: "memory-empty-error"
+          });
+        }
+        break;
     }
   }
 
