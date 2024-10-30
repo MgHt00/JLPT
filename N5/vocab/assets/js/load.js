22cbee5896c9cc68d248f86b4945026ce23d6aee
Mg Htoo <whshwe@gmail.com>
2024-10-30 16:55:14 +0630

    JS: clear sectionStatus when list mistake is clicked

--- N5/vocab/assets/js/load.js
+++ N5/vocab/assets/js/load.js
@@ -759,7 +759,7 @@ function loaderManager() {
   
     const mistakeArray = vocabInstance.loadLocalStorage(); // Load mistakes from localStorage
 
-    clearScreen([sectionQuestion, sectionMessage, sectionAnswer], "fast");
+    clearScreen([sectionStatus, sectionQuestion, sectionMessage, sectionAnswer], "fast");
   
     // Create the container to display the mistakes
     buildNode({
