7a38fae9e155a6f01195e7bd215ae01e6682a42e
Mg Htoo <whshwe@gmail.com>
2024-10-30 15:49:23 +0630

    JS bugfix: remove btns in mistakeList() clearScreen()

--- N5/vocab/assets/js/load.js
+++ N5/vocab/assets/js/load.js
@@ -759,7 +759,7 @@ function loaderManager() {
   
     const mistakeArray = vocabInstance.loadLocalStorage(); // Load mistakes from localStorage
 
-    clearScreen([sectionQuestion, sectionMessage], "fast");
+    clearScreen([sectionQuestion, sectionMessage, sectionAnswer], "fast");
   
     // Create the container to display the mistakes
     buildNode({
