7d1285c917e5d1a1f0f82830159364d73b4ecf3a
Mg Htoo <whshwe@gmail.com>
2025-04-26 11:33:31 +0630

    answerListenerManager: mark locals with the underscore

--- src/js/components/answerListenerManager.js
+++ src/js/components/answerListenerManager.js
@@ -15,20 +15,20 @@ export function answerListnerManager(globals, utilsManager, vocabFns, questionFn
   function handleFlashcardFlip() {
     //console.groupCollapsed("answerManager() - handleFlashcardFlip()");
 
-    removeExistingButtons();
-    fadeMessangeAndAnswer();
+    _removeExistingButtons();
+    _fadeMessangeAndAnswer();
 
     setTimeout(() => {
-      showContent("correctAns");    // Show correct answer
-      showContent("message");       // Show Message
-      showContent("yesNo");         // Show `Yes` `No` buttons
-      fadeMessangeAndAnswer();
+      _showContent("correctAns");    // Show correct answer
+      _showContent("message");       // Show Message
+      _showContent("yesNo");         // Show `Yes` `No` buttons
+      _fadeMessangeAndAnswer();
     }, 350);
 
     console.groupEnd();
 
     // utility functions private to the module
-    function removeExistingButtons() { // Remove exiting buttons
+    function _removeExistingButtons() { // Remove exiting buttons
       const answerButtons = document.querySelectorAll('[id^="answer-btn"]'); // sn3
       answerButtons.forEach(button => {
         displayUtils.toggleClass('fade-out-light', button);
@@ -38,11 +38,11 @@ export function answerListnerManager(globals, utilsManager, vocabFns, questionFn
       });
     }
 
-    function fadeMessangeAndAnswer() {
+    function _fadeMessangeAndAnswer() {
       displayUtils.toggleClass('fade-out-light', selectors.sectionMessage, selectors.sectionAnswer);
     }
 
-    const CONTENT_CONFIG = {
+    const _CONTENT_CONFIG = {
       correctAns: {
         content: appState.correctAns,
         className: 'flash-correct-answer',
@@ -61,20 +61,20 @@ export function answerListnerManager(globals, utilsManager, vocabFns, questionFn
       },
     }
 
-    function showContent(key) {
-      const config = CONTENT_CONFIG[key];
-      if (!config) {
-        console.warn(`handleFlashcardFlip() - showContent() - No config found for key: "${key}"`);
+    function _showContent(key) {
+      const _config = _CONTENT_CONFIG[key];
+      if (!_config) {
+        console.warn(`handleFlashcardFlip() - _showContent() - No _config found for key: "${key}"`);
         return;
       }
 
       domUtils.buildNode({
         parent: selectors.sectionAnswer,
         child: 'div',
-        content: config.content,
-        className: config.className,
-        id: config.id,
-        eventFunction: config.eventFunction
+        content: _config.content,
+        className: _config.className,
+        id: _config.id,
+        eventFunction: _config.eventFunction
       });
     }
   }
@@ -86,19 +86,19 @@ export function answerListnerManager(globals, utilsManager, vocabFns, questionFn
     displayUtils.addClass('fade-hide', selectors.sectionMessage)
                 .addClass('overlay-message', selectors.sectionMessage);
 
-    const btnText = event.currentTarget.textContent;
-    if (appState.correctAns === btnText) {  // If the answer is CORRECT
-      clearScreen("light");
+    const _btnText = event.currentTarget.textContent;
+    if (appState.correctAns === _btnText) {  // If the answer is CORRECT
+      _clearScreen("light");
 
       setTimeout(() => {
-        toggleFadeAndDim("fadeAndDim");
-        showContent("correct");
+        _toggleFadeAndDim("fadeAndDim");
+        _showContent("correct");
       }, 350);
 
       setTimeout(() => {
-        toggleFadeAndDim("fadeAndDim");     // Hide fully
-        clearScreen("deep");
-        checkModeAndRemoveVocab();
+        _toggleFadeAndDim("fadeAndDim");     // Hide fully
+        _clearScreen("deep");
+        _checkModeAndRemoveVocab();
         finalizeQuestionAndProceed(true);
       }, 1200);                             // Add delay equal to the fade-out transition duration
     } 
@@ -110,11 +110,11 @@ export function answerListnerManager(globals, utilsManager, vocabFns, questionFn
         domUtils.clearScreen(selectors.sectionMessage);
 
         setTimeout(() => {
-          showContent("incorrect");         // Show overlay "wrong" message
-          toggleFadeAndDim("fadeOnly");
+          _showContent("incorrect");         // Show overlay "wrong" message
+          _toggleFadeAndDim("fadeOnly");
 
           setTimeout(() => {                 // Fully hide after fade-out completes (1s from .fade-out transition)
-            toggleFadeAndDim("fadeOnly");    // Hide fully
+            _toggleFadeAndDim("fadeOnly");    // Hide fully
           }, 1000);                          // Add delay equal to the fade-out transition duration
         }, 350);
            
@@ -122,7 +122,7 @@ export function answerListnerManager(globals, utilsManager, vocabFns, questionFn
     console.groupEnd();
 
     // utility functions private to the module
-    function getContentConfig() {
+    function _getContentConfig() {
       return {
         correct: {
           content: 'Correct',
@@ -135,17 +135,17 @@ export function answerListnerManager(globals, utilsManager, vocabFns, questionFn
       }
     }
 
-    function showContent(key) {
-      const config = getContentConfig()[key];
+    function _showContent(key) {
+      const _config = _getContentConfig()[key];
       domUtils.buildNode({ 
         parent: selectors.sectionMessage, 
         child: 'div', 
-        content: config.content, 
-        className: config.className 
+        content: _config.content, 
+        className: _config.className 
       });
     }
 
-    function getClearConfig() {             // To avoide JS temporal dead zone (TDZ)
+    function _getClearConfig() {             // To avoide JS temporal dead zone (TDZ)
       return {
         light: { 
           target: selectors.sectionMessage,
@@ -159,9 +159,9 @@ export function answerListnerManager(globals, utilsManager, vocabFns, questionFn
       };
     }
 
-    function clearScreen(mode) {
-      const config = getClearConfig()[mode]; // [sn26] 
-      domUtils.clearScreen(config.target);
+    function _clearScreen(mode) {
+      const _config = _getClearConfig()[mode]; // [sn26] 
+      domUtils.clearScreen(_config.target);
     }
   }
 
@@ -169,14 +169,14 @@ export function answerListnerManager(globals, utilsManager, vocabFns, questionFn
   function handleFlashCardYesNoAnswer(event) { // sn4
     //console.groupCollapsed("answerManager() - handleFlashCardYesNoAnswer()");
     
-    const btnID = event.currentTarget.id;
+    const _btnID = event.currentTarget.id;
 
-    if (btnID === "choice-btn-0") {
-      checkModeAndRemoveVocab();
+    if (_btnID === "choice-btn-0") {
+      _checkModeAndRemoveVocab();
       finalizeQuestionAndProceed(true);
     } 
     
-    else if (btnID === "choice-btn-1") {
+    else if (_btnID === "choice-btn-1") {
       if (readQuestionMode() !== "stored") {
         storeToMistakeBank(); // add wrongly selected word to localstorage
       } 
@@ -191,18 +191,18 @@ export function answerListnerManager(globals, utilsManager, vocabFns, questionFn
   function handleContinueToStoredData(event) {
     console.groupCollapsed("answerManager() - handleContinueToStoredData()");
 
-    toggleFadeAndDim("overlay");
+    _toggleFadeAndDim("overlay");
 
-    const btnID = event.currentTarget.id;
+    const _btnID = event.currentTarget.id;
 
-    if (btnID === "continueYes-0") {
+    if (_btnID === "continueYes-0") {
       console.info("Clicked Yes");
       setQuestionMode("stored");
       _setRanOnce(true); // set true to `ranOnce` so that when storedData complete, continue to stored data will not show again.
       continuetoStoredData();
     } 
     
-    else if (btnID === "continueNo-0") {
+    else if (_btnID === "continueNo-0") {
       console.info("Clicked No");
       _setRanOnce(false);
       restart();
@@ -211,43 +211,43 @@ export function answerListnerManager(globals, utilsManager, vocabFns, questionFn
   }
 
   // Check q mode and decide whether to proceed to remove from the mistake bank
-  function checkModeAndRemoveVocab() {
-    console.groupCollapsed("checkModeAndRemoveVocab()");
+  function _checkModeAndRemoveVocab() {
+    console.groupCollapsed("_checkModeAndRemoveVocab()");
     
-    const currentQuestionMode = readQuestionMode();
+    const _currentQuestionMode = readQuestionMode();
 
-    if (currentQuestionMode === "stored") { // if current q mode is stored and answer is right
-      console.info("currentQuestionMode: ", currentQuestionMode, ".  removeFromMistakeBank() is called.");
+    if (_currentQuestionMode === "stored") { // if current q mode is stored and answer is right
+      console.info("_currentQuestionMode: ", _currentQuestionMode, ".  removeFromMistakeBank() is called.");
       removeFromMistakeBank();
     } else {
-      console.info("currentQuestionMode: ", currentQuestionMode, ".  No need to remove mistakes");
+      console.info("_currentQuestionMode: ", _currentQuestionMode, ".  No need to remove mistakes");
     }
 
     console.groupEnd();
   }
 
-  function toggleFadeAndDim(key) {
-    const shouldFade = [ "fadeOnly", "fadeAndDim", "overlay"].includes(key); 
-    const shouldDim = key === "fadeAndDim";
-    const shouldOverlay = key === "overlay";
+  function _toggleFadeAndDim(key) {
+    const _shouldFade = [ "fadeOnly", "fadeAndDim", "overlay"].includes(key); 
+    const _shouldDim = key === "fadeAndDim";
+    const _shouldOverlay = key === "overlay";
     // If key: "fadeOnly", "fadeAndDim", or "overlay" => toggle 'fade-hide'
     // key: "fadeAndDim" => toggle 'so-dim'
     // key: "overlay" => toggle 'overlay-message'
     
-    if (shouldFade) {
+    if (_shouldFade) {
       displayUtils.toggleClass('fade-hide', selectors.sectionMessage);
     }
 
-    if (shouldDim) {
+    if (_shouldDim) {
       displayUtils.toggleClass('so-dim', selectors.sectionStatus, selectors.sectionAnswer);
     }
 
-    if (shouldOverlay) {
+    if (_shouldOverlay) {
       displayUtils.toggleClass('overlay-message', selectors.sectionMessage);
     }
 
-    if (!shouldFade && !shouldDim && !shouldOverlay) {
-      console.warn(`handleMultipleChoiceAnswer() - toggleFadeAndDim() - invalid key: "${key}"`);
+    if (!_shouldFade && !_shouldDim && !_shouldOverlay) {
+      console.warn(`handleMultipleChoiceAnswer() - _toggleFadeAndDim() - invalid key: "${key}"`);
     }
   }
 
