930f4101f7a1e49aa0a0fe7eab8e2b92cb7413dd
Mg Htoo <whshwe@gmail.com>
2025-04-26 12:11:20 +0630

    selectors.js: separate HTML element ids with ELEMENTIDS.js

---
+++ src/js/constants/elementIDs.js
@@ -0,0 +1,41 @@
+export const ELEMENTIDS = {
+  SECTION_QUESTION: "#section-question",
+  SECTION_MESSAGE: "#section-message",
+  SECTION_ANSWER: "#section-answer",
+  SECTION_STATUS: "#section-status",
+
+  DYNAMIC_DOM: "#dynamic-DOM",
+
+  SETTING_FORM: "#settingsForm",
+  SETTING_FLASH_YES_NO: "#settings-flashYesNo",
+  SETTING_NO_OF_ANS: "#settings-noOfAnswers",
+  SETTING_SOURCE: "#settings-source",
+  SETTING_SYLLABLE: "#settings-syllableChoice",
+  SETTING_REPRACTICE: "#settings-repractice",
+
+  SWITCH_RANDOM_YES_NO: "#randomYesNo",
+  SWITCH_FLASH_YES_NO: "#flashYesNo",
+  QUESTION_CHOICE: "#qChoiceInput",
+  ANSWER_CHOICE: "#aChoiceInput",
+  FIELDSET_SYLLABLE: "#fieldset-syllable",
+  MEMORY_INFO: "#memory-info",
+  MEMORY_BTNS: "#memory-btns",
+  ANSWER_CHOICE_SELECTOR_ALL: "[id^='aChoiceInput']",
+  ANSWER_CHOICE_OPTION_ALL: 'select[id="aChoiceInput"]',
+  ANSWER_DEFAULT_CHOICE: 'select[name="aChoiceInput"]',
+  NO_OF_ANSWER_ALL: "[id^='noOfAnswers']",
+  SUBMIT_BTN: "#submit-btn",
+  ALL_SETTING: "[id|='settings']",
+  BRING_BACK_BTN: "#bring-back-btn",
+  RESUME_PRACTICE_BTN: "#resume-practice-btn",
+  BRING_BACK_BTN_CONTAINER: "#bring-back-btn-container",
+
+  NO_OF_ANSWER_CHECKED: 'input[name="noOfAnswers"]:checked',
+  RANDOM_YES_NO_CHECKED: 'input[name="randomYesNo"]:checked',
+  FLASH_YES_NO_CHECKED: 'input[name="flashYesNo"]:checked',
+  SOURCE_CHECKED: 'input[name="source"]:checked',
+
+  MEMORY_INFO_ZERO: "#memory-info-0",
+  FLUSH_MEMORY_BTN_ZERO: "#flush-memory-btn-0",
+  LIST_MEMORY_BTN_ZERO: "#list-memory-btn-0",
+}
