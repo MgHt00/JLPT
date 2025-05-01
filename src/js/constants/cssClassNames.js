4b894bc30b4f1984976833ae1f531586efacc6a6
Mg Htoo <whshwe@gmail.com>
2025-05-01 12:13:54 +0630

    controlManager: changing hardcoded variable with constants

--- src/js/constants/cssClassNames.js
+++ src/js/constants/cssClassNames.js
@@ -7,8 +7,13 @@ export const LANG_CLASS_NAMES = {
 
 export const CSS_CLASS_NAMES = {
   SHIFT_TO_TOP_CENTER: 'shift-sections-to-top-center',
+  SHIFT_TO_CENTER: 'shift-sections-to-center',
 
   MEMORY_INFO: 'memory-info',
   SHOW: 'show',
   HIDE: 'hide',
+  MOVED: 'moved',
+  DISABLED: 'disabled',
+  DIM: 'dim',
+  SHADES_ON_TOP: 'shades-on-top',
 };
